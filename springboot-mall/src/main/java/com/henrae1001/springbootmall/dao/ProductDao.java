089d38639e9d818866c7b717cc05c277d65c9370
吳翰宇 <henrywu5421@gmail.com>
2024-10-15 15:38:40 +0800

    商品功能 - 實作修改商品

--- springboot-mall/src/main/java/com/henrae1001/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/henrae1001/springbootmall/dao/ProductDao.java
@@ -5,6 +5,7 @@ import com.henrae1001.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
-
     Integer createProduct(ProductRequest productRequest);
+    void updateProduct(Integer productId,ProductRequest productRequest);
+
 }
