379f7717012d3cecc9b0ac1d1e998084ff519b86
吳翰宇 <henrywu5421@gmail.com>
2024-10-15 23:22:50 +0800

    商品功能 - 實作刪除商品

--- springboot-mall/src/main/java/com/henrae1001/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/henrae1001/springbootmall/dao/ProductDao.java
@@ -8,4 +8,5 @@ public interface ProductDao {
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId,ProductRequest productRequest);
 
+    void deleteProductById(Integer productId);
 }
