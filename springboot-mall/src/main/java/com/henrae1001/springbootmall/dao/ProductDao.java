4167c75d4f8728380873e89c34a3d852c2907abf
吳翰宇 <henrywu5421@gmail.com>
2024-10-16 14:20:53 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/henrae1001/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/henrae1001/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.henrae1001.springbootmall.dao;
 import com.henrae1001.springbootmall.dto.ProductRequest;
 import com.henrae1001.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId,ProductRequest productRequest);
--- springboot-mall/src/main/java/com/henrae1001/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/henrae1001/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.henrae1001.springbootmall.service;
 import com.henrae1001.springbootmall.dto.ProductRequest;
 import com.henrae1001.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
