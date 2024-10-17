cd5527e4ed467b79c252344cb363f58262a2a901
吳翰宇 <henrywu5421@gmail.com>
2024-10-17 13:50:34 +0800

    商品功能 - 實作查詢商品列表(分則)

--- springboot-mall/src/main/java/com/henrae1001/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/henrae1001/springbootmall/controller/ProductController.java
@@ -6,15 +6,19 @@ import com.henrae1001.springbootmall.dto.ProductRequest;
 import com.henrae1001.springbootmall.model.Product;
 import com.henrae1001.springbootmall.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.parsing.Problem;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
     @Autowired
@@ -27,13 +31,19 @@ public class ProductController {
 
         //排序 Sorting
         @RequestParam(defaultValue = "created_date") String orderby,
-        @RequestParam(defaultValue = "desc") String sort
+        @RequestParam(defaultValue = "desc") String sort,
+
+        //分頁 Pagination
+        @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+        @RequestParam(defaultValue = "0") @Min(0) Integer offset
     ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderby(orderby);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
