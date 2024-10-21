bda671a94d2bb8eee60990e66ebec8eb7048a6d7
吳翰宇 <henrywu5421@gmail.com>
2024-10-21 10:04:27 +0800

    帳號功能 - 登入

--- springboot-mall/src/main/java/com/henrae1001/springbootmall/service/UserService.java
+++ springboot-mall/src/main/java/com/henrae1001/springbootmall/service/UserService.java
@@ -1,9 +1,12 @@
 package com.henrae1001.springbootmall.service;
 
+import com.henrae1001.springbootmall.dto.UserLoginRequest;
 import com.henrae1001.springbootmall.dto.UserRegisterRequest;
 import com.henrae1001.springbootmall.model.User;
 
 public interface UserService {
     User getUserById(Integer userId);
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
